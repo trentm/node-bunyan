99ecaf2845e166579bd022f1c04fe7cb78e39511
Trent Mick <trentm@gmail.com>
2012-01-30 12:01:15 -0800

    log.clone

--- TODO.md
+++ TODO.md
@@ -1,10 +1,15 @@
-- node-bunyan and push to priv
-- .bind or equiv with diff name
 - info's siblings
 - `bunyan` cli
 - expand set of fields
+    <https://github.com/Graylog2/graylog2-docs/wiki/GELF>
+    <http://journal.paul.querna.org/articles/2011/12/26/log-for-machines-in-json/>
+    require: facility and hostname
 - renderer support (i.e. repr of a restify request obj)
 - docs
 - feel out usage
+- not sure about `log.info()` for is-enabled. Perhaps `log.isInfo()` because
+  can then use that for `log.isInfo(true)` for 'ring' argument. Separate level
+  and ringLevel.
 - Logger.set to mutate config or `this.fields`
 - Logger.del to remove a field
+- test suite
--- hi.js
+++ hi.js
@@ -1,4 +1,5 @@
 var Logger = require('./lib/bunyan');
+
 var log = new Logger({facility: "myapp", level: "info"});
 console.log("log.info() is:", log.info())
 log.info("hi");
@@ -9,43 +10,16 @@ log.info({foo:"bar"}, "hi %d", 1, "two", 3);
 
 console.log("\n--\n")
 
+function Wuzzle(options) {
+  this.log = options.log;
+  this.log.info("creating a wuzzle")
+}
 
-//console.log("\n--\n")
-//xxx = console.log;
-//var INFO = 2;
-//
-//function foo() {
-//  console.log("function foo arguments:", arguments)
-//}
-//
-//function Bar() {
-//  this.level = 1;
-//}
-//Bar.prototype.info = function (fields, msg) {
-//  console.log("function info arguments:", arguments)
-//  var msgArgs;
-//  if (arguments.length === 0) {             // `log.info()`
-//    return (this.level <= INFO);
-//  } else if (this.level > INFO) {
-//    return;
-//  } else if (typeof fields === 'string') {  // `log.info(msg, ...)`
-//    fields = null;
-//    msgArgs = Array.prototype.slice.call(arguments);
-//    xxx("msgArgs: ", msgArgs, arguments)
-//  } else {                                  // `log.info(fields, msg, ...)`
-//    msgArgs = Array.prototype.slice.call(arguments, 1);
-//  }
-//  xxx("info start: arguments:", arguments.length, arguments, msgArgs)
-//  //var rec = this._mkRecord(fields, INFO, msgArgs);
-//  //this._emit(rec);
-//}
-//
-//
-//foo()
-//foo("one")
-//foo("one", "two")
-//
-//bar = new Bar();
-//bar.info()
-//bar.info("one")
-//bar.info("one", "two")
+Wuzzle.prototype.woos = function () {
+  this.log.info("This wuzzle is woosey.")
+}
+
+var wuzzle = new Wuzzle({log: log.clone({component: "wuzzle"})});
+wuzzle.woos();
+log.info("done with the wuzzle")
+
