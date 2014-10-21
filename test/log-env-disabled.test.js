a3dea7d61723562e80385f941efef44fb03b15ea
Marcello de Sales <Marcello_DeSales@intuit.com>
2014-10-20 17:15:51 -0700

    Initial commit to fix #175 accompained by test cases.
    This commit adds the function that initially fixes #175 by disabling the
    record being emitted. Requirements are described in the issue #175.
    
    *	modified:   lib/bunyan.js
    - Adding a helper method to disable objects by properties
    
    *	new file:   test/log-env-disabled.test.js
    - Adding the disable test case by setting the environment var before loading
      the record.

---
+++ test/log-env-disabled.test.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2014 Trent Mick. All rights reserved.
+ *
+ */
+
+// Records that has this name will NOT be emitted.
+process.env.BUNYAN_REC_DISABLE_name = "to-be-disabled";
+
+var util = require('util'),
+    format = util.format,
+    inspect = util.inspect;
+var p = console.log;
+
+var bunyan = require('../lib/bunyan');
+
+// node-tap API
+if (require.cache[__dirname + '/tap4nodeunit.js'])
+        delete require.cache[__dirname + '/tap4nodeunit.js'];
+var tap4nodeunit = require('./tap4nodeunit.js');
+var after = tap4nodeunit.after;
+var before = tap4nodeunit.before;
+var test = tap4nodeunit.test;
+
+
+// ---- test boolean `log.<level>()` calls
+
+var log1 = bunyan.createLogger({
+    name: 'log1',
+    streams: [
+        {
+            path: __dirname + '/log.test.log1.log',
+            level: 'info'
+        }
+    ]
+});
+
+var log2 = bunyan.createLogger({
+    name: 'log2',
+    streams: [
+        {
+            path: __dirname + '/log.test.log2a.log',
+            level: 'error'
+        },
+        {
+            path: __dirname + '/log.test.log2b.log',
+            level: 'debug'
+        }
+    ]
+})
+
+// ---- test `log.<level>(...)` calls which various input types
+
+function Catcher() {
+    this.records = [];
+}
+Catcher.prototype.write = function (record) {
+    this.records.push(record);
+}
+var catcher = new Catcher();
+var log3 = new bunyan.createLogger({
+    name: 'to-be-disabled',
+    streams: [
+        {
+            type: 'raw',
+            stream: catcher,
+            level: 'trace'
+        }
+    ]
+});
+
+var names = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'];
+
+test('log.info(null, disabled by "name")', function (t) {
+    names.forEach(function (lvl) {
+        log3[lvl].call(log3, null, 'some message');
+        var rec = catcher.records[catcher.records.length - 1];
+        t.notEqual(rec, 'undefined');
+    });
+    delete process.env.BUNYAN_REC_DISABLE_name;
+    t.end();
+});
