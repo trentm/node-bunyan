09323ac7855bcc8c26628b4d5baf77c7bb84d462
Nathan <nathan.hyman@coatesgroup.com>
2017-07-04 16:54:24 +1000

    Add 'n' depth stack for src attribute to allow for wrapped logs Related to #72

--- test/src.test.js
+++ test/src.test.js
@@ -7,6 +7,13 @@
 // Intentionally on line 8 for tests below:
 function logSomething(log) { log.info('something'); }
 
+function someFunc(log) {
+    log.customLevel = function () {
+        log.info('something else');
+    }
+    // Intentionally on line 15 for tests below:
+    log.customLevel();
+}
 
 var format = require('util').format;
 var Logger = require('../lib/bunyan');
@@ -42,10 +49,14 @@ test('src', function (t) {
         ]
     });
 
+    t.equal(log.src, 2);
+
     log.info('top-level');
     logSomething(log);
+    log.src = 3;
+    someFunc(log);
 
-    t.equal(recs.length, 2);
+    t.equal(recs.length, 3);
     recs.forEach(function (rec) {
         t.ok(rec.src);
         t.equal(typeof (rec.src), 'object');
@@ -57,6 +68,10 @@ test('src', function (t) {
     t.ok(rec.src.func);
     t.equal(rec.src.func, 'logSomething');
     t.equal(rec.src.line, 8);
+    var rec = recs[2];
+    t.ok(rec.src.func);
+    t.equal(rec.src.func, 'someFunc');
+    t.equal(rec.src.line, 15);
 
     t.end();
 });
